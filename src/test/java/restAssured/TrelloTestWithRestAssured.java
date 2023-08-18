ce3bb60bbeca9d4dfc757599cd034f5e3c4b9857
Andrei Novosiolov <anovosiolov@griddynamics.com>
2023-08-18 15:08:45 +0300

    allure description and steps

--- src/test/java/restAssured/TrelloTestWithRestAssured.java
+++ src/test/java/restAssured/TrelloTestWithRestAssured.java
@@ -1,5 +1,6 @@
 package restAssured;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.testng.AllureTestNg;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -11,6 +12,7 @@ import restAssured.testStepsLib.GetBoardSteps;
 @Listeners({AllureTestNg.class})
 public class TrelloTestWithRestAssured {
     @Test(priority = 1)
+    @Description("Test to verify POST request")
     public void createBoardTest() {
         CreateBoardSteps steps = new CreateBoardSteps();
 
@@ -21,6 +23,7 @@ public class TrelloTestWithRestAssured {
         steps.assertResponse();
     }
     @Test(priority = 2)
+    @Description("Test to verify GET request")
     public void getBoardTest() {
         GetBoardSteps steps = new GetBoardSteps();
 
@@ -31,6 +34,7 @@ public class TrelloTestWithRestAssured {
         steps.assertResponse();
     }
     @Test(priority = 3)
+    @Description("Test to verify PUT request")
     public void changeBoardNameTest() {
         ChangeBoardNameStep steps = new ChangeBoardNameStep();
 
@@ -41,6 +45,7 @@ public class TrelloTestWithRestAssured {
         steps.assertResponse();
     }
     @Test(priority = 4)
+    @Description("Test to verify DELETE request")
     public void deleteBoardTest() {
         DeleteBoardStep steps = new DeleteBoardStep();
 
--- src/test/java/retrofit/TrelloTestsWithRetrofit.java
+++ src/test/java/retrofit/TrelloTestsWithRetrofit.java
@@ -1,5 +1,6 @@
 package retrofit;
 
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 import io.qameta.allure.testng.AllureTestNg;
 import org.testng.annotations.Listeners;
@@ -12,6 +13,7 @@ import java.io.IOException;
 @Listeners({AllureTestNg.class})
 public class TrelloTestsWithRetrofit extends BaseTestRetrofit {
     @Test(priority = 1)
+    @Description("Test to verify POST request")
     public void createBoardTest() throws IOException {
 
         CreateBoard steps = new CreateBoard();
@@ -22,6 +24,7 @@ public class TrelloTestsWithRetrofit extends BaseTestRetrofit {
     }
 
     @Test(priority = 2)
+    @Description("Test to verify GET request")
     public void getBoardTest() throws IOException {
         GetBoard steps = new GetBoard();
         steps.initializeProperties();
@@ -30,6 +33,7 @@ public class TrelloTestsWithRetrofit extends BaseTestRetrofit {
         steps.assertResponse();
     }
     @Test (priority = 3)
+    @Description("Test to verify PUT request")
     public void changeBoardName () throws IOException {
         ChangeName steps = new ChangeName();
         steps.initializeProperties();
@@ -38,6 +42,7 @@ public class TrelloTestsWithRetrofit extends BaseTestRetrofit {
         steps.assertResponse();
     }
     @Test (priority = 4)
+    @Description("Test to verify DELETE request")
     public void deleteBoard () throws IOException{
         DeleteBoard steps = new DeleteBoard();
         steps.initializeProperties();
